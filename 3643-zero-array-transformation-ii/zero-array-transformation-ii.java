38e4d8706508caeb70c1cf56bb9ee42fdcbabad8
Vipul Kumar <38461068+Vipulhere@users.noreply.github.com>
2025-03-14 17:58:07 +0530

    Time: 25 ms (42.09%) | Memory: 123.4 MB (40.39%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -1,38 +1,46 @@
 class Solution {
-    private int n;
-    private int[] nums;
-    private int[][] queries;
+    //Github -- vipulhere
+    private int n; // Stores the length of the nums array
+    private int[] nums; // Stores the input nums array
+    private int[][] queries; // Stores the queries array
 
     public int minZeroArray(int[] nums, int[][] queries) {
-        this.nums = nums;
-        this.queries = queries;
-        n = nums.length;
-        int m = queries.length;
-        int l = 0, r = m + 1;
+        this.nums = nums; // Store nums array in class variable
+        this.queries = queries; // Store queries in class variable
+        n = nums.length; // Get the size of the nums array
+        int m = queries.length; // Get the number of queries
+        int l = 0, r = m + 1; // Initialize binary search range
+
+        // Binary search to find the minimum number of queries required
         while (l < r) {
-            int mid = (l + r) >> 1;
-            if (check(mid)) {
-                r = mid;
+            int mid = (l + r) >> 1; // Find mid-point of search range
+            if (check(mid)) { // If it's possible to make nums a zero array within mid queries
+                r = mid; // Try reducing the number of queries
             } else {
-                l = mid + 1;
+                l = mid + 1; // Otherwise, increase the query count
             }
         }
-        return l > m ? -1 : l;
+        return l > m ? -1 : l; // If l exceeds the number of queries, return -1; otherwise, return l
     }
 
+    // Function to check if it's possible to make nums a zero array within k queries
     private boolean check(int k) {
-        int[] d = new int[n + 1];
+        int[] d = new int[n + 1]; // Difference array to track range updates efficiently
+
+        // Apply the first k queries to the difference array
         for (int i = 0; i < k; ++i) {
             int l = queries[i][0], r = queries[i][1], val = queries[i][2];
-            d[l] += val;
-            d[r + 1] -= val;
+            d[l] += val; // Increment the left index
+            d[r + 1] -= val; // Decrement the right index (marking the end of range update)
         }
+
+        // Apply the difference array to check if all elements can be reduced to zero
         for (int i = 0, s = 0; i < n; ++i) {
-            s += d[i];
-            if (nums[i] > s) {
-                return false;
+            s += d[i]; // Compute the prefix sum to apply the decrement
+            if (nums[i] > s) { // If the decrement isn't enough to make nums[i] zero
+                return false; // Not possible within k queries
             }
         }
-        return true;
+        return true; // All elements can be reduced to zero
     }
 }
