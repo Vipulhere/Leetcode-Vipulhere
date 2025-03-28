3e0f38121a0e78cae7ed49e38e2b3d7fac64d255
Vipul Kumar <38461068+Vipulhere@users.noreply.github.com>
2025-03-28 13:19:38 +0530

    Time: 102 ms (47.33%) | Memory: 58 MB (54.20%) - LeetSync

--- 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.java
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.java
@@ -1,54 +1,61 @@
 class Solution {
     public int[] maxPoints(int[][] grid, int[] queries) {
         int m = grid.length, n = grid[0].length;
-        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}}; // directions for traversal
+        //Github -- Vipulhere
+        // Directions: right, down, left, up
+        int[][] dirs = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+        // Result array to store max points for each query
         int[] res = new int[queries.length];
 
-        // Create a list of query indices to sort and map result later
+        // Create a new array that stores both the query value and original index
+        // so we can sort the queries and later place the result in the correct index
         int[][] indexedQueries = new int[queries.length][2];
         for (int i = 0; i < queries.length; i++) {
-            indexedQueries[i][0] = queries[i];
-            indexedQueries[i][1] = i;
+            indexedQueries[i][0] = queries[i]; // query value
+            indexedQueries[i][1] = i;          // original index
         }
+
+        // Sort queries by their value so we can process them efficiently
         Arrays.sort(indexedQueries, (a, b) -> Integer.compare(a[0], b[0]));
 
+        // Visited array to mark which cells have been added to the queue
         boolean[][] visited = new boolean[m][n];
+
+        // Min-heap priority queue to explore smallest grid values first
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
-        pq.offer(new int[]{grid[0][0], 0, 0}); // [value, x, y]
+        
+        // Start from top-left corner if it is valid
+        pq.offer(new int[]{grid[0][0], 0, 0}); // [cell value, x, y]
         visited[0][0] = true;
 
-        int count = 0; // number of cells visited so far
-        int[][] temp = new int[m][n];
+        // Count of how many valid cells we've visited (i.e., collected points from)
+        int count = 0;
 
-        while (!pq.isEmpty() && pq.peek()[0] < indexedQueries[0][0]) {
-            int[] cur = pq.poll();
-            int val = cur[0], x = cur[1], y = cur[2];
-            temp[x][y] = 1;
-            count++;
+        // Used to mark cells we've already counted points from
+        int[][] counted = new int[m][n];
 
-            for (int[] d : dirs) {
-                int nx = x + d[0], ny = y + d[1];
-                if (nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    pq.offer(new int[]{grid[nx][ny], nx, ny});
-                }
-            }
-        }
+        // Loop through all queries in increasing order
+        for (int i = 0; i < indexedQueries.length; i++) {
+            int queryVal = indexedQueries[i][0];
+            int originalIdx = indexedQueries[i][1];
 
-        int prevCount = count;
-        int qIdx = 0;
+            // Process all cells in the priority queue with value less than queryVal
+            while (!pq.isEmpty() && pq.peek()[0] < queryVal) {
+                int[] curr = pq.poll();
+                int val = curr[0], x = curr[1], y = curr[2];
 
-        for (int i = 0; i < queries.length; i++) {
-            int q = indexedQueries[i][0];
-            while (!pq.isEmpty() && pq.peek()[0] < q) {
-                int[] cur = pq.poll();
-                int val = cur[0], x = cur[1], y = cur[2];
-                if (temp[x][y] == 0) {
-                    temp[x][y] = 1;
-                    count++;
+                // If we haven't already counted this cell
+                if (counted[x][y] == 0) {
+                    counted[x][y] = 1; // Mark it counted
+                    count++; // Increase point count
 
-                    for (int[] d : dirs) {
-                        int nx = x + d[0], ny = y + d[1];
+                    // Check all 4 directions for valid adjacent cells
+                    for (int[] dir : dirs) {
+                        int nx = x + dir[0];
+                        int ny = y + dir[1];
+
+                        // If adjacent cell is in bounds and not visited
                         if (nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[nx][ny]) {
                             visited[nx][ny] = true;
                             pq.offer(new int[]{grid[nx][ny], nx, ny});
@@ -57,7 +64,8 @@ class Solution {
                 }
             }
 
-            res[indexedQueries[i][1]] = count;
+            // Store the number of points collected for this query
+            res[originalIdx] = count;
         }
 
         return res;
