a65c55e26c5156db8591e24d40d914e7c77da6e6
Vipul Kumar <38461068+Vipulhere@users.noreply.github.com>
2025-03-31 15:27:03 +0530

    Time: 101 ms (17.16%) | Memory: 55.6 MB (96.27%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.java
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.java
@@ -1,12 +1,10 @@
-import java.util.*;
-
 class Solution {
     public long putMarbles(int[] weights, int k) {
         int n = weights.length;
         if (k == 1 || k == n) return 0;
 
         List<Integer> pairSums = new ArrayList<>();
-
+        //Github -- vipulhere
         // Calculate the cost of making a cut between each adjacent pair
         for (int i = 0; i < n - 1; i++) {
             pairSums.add(weights[i] + weights[i + 1]);
